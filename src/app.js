18c04cad96eb762a723b027431363e3d6bad7e16
JuanPVargas <juanvarji@unisabana.edu.co>
2024-10-09 11:43:35 -0500

    creacion estructura

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-//hola
+import express from 'express';
+
+//servidor
+const app = express();
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import app from './app.js'
+
+//corre en el puerto 4000
+app.listen(4000)
+console.log('Server on port', 4000)
