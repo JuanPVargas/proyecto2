e35d0eb62bf13cf8abf571db739e588de94ad1a3
John Jairo Rojas <79581520+JohnRojas06289@users.noreply.github.com>
2024-10-09 16:47:50 -0500

    merge rama john-main

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import app from 'express';
 
 //servidor
 const app = express();
---
+++ src/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try{
+        await mongoose.connect('mongodb+srv://inmobiliaria:<db_password>@cluster0.hym6w.mongodb.net/')
+        console.log('DB is connected')
+    } catch (error) {
+        console.error('Error connecting to MongoDB: ', error);
+    }
+};
