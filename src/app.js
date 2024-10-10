8f848120f4233fab6bb63e7fb89afd7ab24c8c04
John Jairo Rojas <79581520+JohnRojas06289@users.noreply.github.com>
2024-10-10 10:41:04 -0500

    conexión db local y creación de usuarios

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
+
+import authRoutes from './routes/auth.routes.js';
 
 //servidor
 const app = express();
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.use('/api', authRoutes);
 
 export default app;
--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect('mongodb+srv://inmobiliaria:admin@cluster0.hym6w.mongodb.net/')
+        await mongoose.connect('mongodb://localhost:27017')
         console.log('DB is connected')
     } catch (error) {
         console.error('Error connecting to MongoDB: ', error);
