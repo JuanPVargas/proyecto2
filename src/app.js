72fe31bc17023b951f7f6df4017966fefd5a6cd2
JuanPVargas <juanvarji@unisabana.edu.co>
2024-10-09 19:36:07 -0500

    app_js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
 
-//import authRoutes from './routes/auth.routes.js'
-
-
-const app = express();
-app.use(morgan('dev'));
 
 //servidor
+const app = express();
+
+app.use(morgan('dev'));
+
 export default app;
