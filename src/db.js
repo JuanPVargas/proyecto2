639a37c9ef673d5d4b1ef1673d050b88e2221eee
JuanPVargas <juanvarji@unisabana.edu.co>
2024-10-10 08:36:38 -0500

    funcionando ThunderClient

--- src/db.js
+++ src/db.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect('mongodb+srv://inmobiliaria:admin@cluster0.hym6w.mongodb.net/')
+        await mongoose.connect('mongodb+srv://Santafe2005:Santafe2005@cluster0.jn84s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log('DB is connected')
     } catch (error) {
         console.error('Error connecting to MongoDB: ', error);
     }
 };
+
